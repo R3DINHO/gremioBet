6af58332756863d72271ca56e42bfdae5f64f266
Redinho <rafaelandres910819@gmail.com>
2025-02-19 14:15:57 -0300

    mudei as ursl

--- gremioBet/settings.py
+++ gremioBet/settings.py
@@ -166,3 +166,20 @@ else:
 
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
