99556082851f2934e99d8cabc5bae1491a92c0b9
Redinho <rafaelandres910819@gmail.com>
2025-02-22 12:18:25 -0300

    mudei as ursl

--- apostas/static/js/main.js
+++ apostas/static/js/main.js
@@ -1,35 +1,48 @@
+// senha
+
 let passwordInputs = document.querySelectorAll('.password-container input');
 
 passwordInputs.forEach(function(input) {
-    // Bloquear o evento de copiar (Ctrl+C)
     input.addEventListener('copy', function(event) {
-        event.preventDefault(); // Impede a ação de copiar
+        event.preventDefault(); 
     });
 
-    // Bloquear o evento de colar (Ctrl+V)
     input.addEventListener('paste', function(event) {
-        event.preventDefault(); // Impede a ação de colar
+        event.preventDefault(); 
     });
 });
 
 
-// Seleciona todos os inputs do tipo radio
+
+// selecionar imagem de perfil
 const radioButtons = document.querySelectorAll('input[type="radio"]');
 
-// Para cada botão de rádio
 radioButtons.forEach(radio => {
-    // Adiciona um event listener para quando o radio for alterado
     radio.addEventListener('change', function() {
-        // Remove a borda de todos os containers de imagem
         const imageContainers = document.querySelectorAll('.image-container');
         imageContainers.forEach(container => {
             container.style.border = '2px solid transparent'; // Borda invisível
         });
 
-        // Adiciona a borda no contêiner de imagem selecionado
         const selectedLabel = this.closest('label');
         const selectedContainer = selectedLabel.querySelector('.image-container');
         selectedContainer.style.border = '2px solid #e46e00'; // Cor da borda
     });
 });
 
+// menu Usuário
+
+function menuUsuario(){
+    let divMenu = document.getElementById("menuUsuarioAbsolute");
+    
+    if(!divMenu.classList.contains("visivel")){
+        divMenu.classList.remove("invisivel")
+        divMenu.classList.add("visivel")
+    }
+    else if(divMenu.classList.contains("visivel")){
+        divMenu.classList.remove("visivel")
+        divMenu.classList.add("invisivel")
+
+    }
+
+}
