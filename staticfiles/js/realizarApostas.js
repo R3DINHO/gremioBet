939c9a4251fe48947aba2900d4dae9a56e8845e7
Redinho <rafaelandres910819@gmail.com>
2025-03-03 20:31:50 -0300

    popup aposta rapida

--- staticfiles/js/realizarApostas.js
+++ staticfiles/js/realizarApostas.js
@@ -1,16 +1,17 @@
+let oddSelecionada = 0;
+
 function apostaRapida(button) {
     document.getElementById("overlay").style.display = "block";
-    document.getElementById("apostaRapida-container").style.display = "block";
+    document.getElementById("apostaRapida-container").style.display = "flex";
 
     let id = button.id; 
     let dadosPartida = document.getElementById("dadosPartida");
-    let oddSelecionada = parseInt(button.innerText.trim()); // Valor da odd
-    let spanRetornoEsperado = document.getElementById("retornoEsperado");
 
-    // Obtém a partida selecionada com base no ID do botão
+    oddSelecionada = getButtonNumber(button); 
+    document.getElementById("exibirOdd").innerText = `Odd: ${oddSelecionada}x`;
+
     let partidaSelecionada = document.querySelector(`.partida[data-id="${id}"]`);
 
-    // Se a partida selecionada existir, preenche os dados da partida
     if (partidaSelecionada) {
         dadosPartida.innerHTML = `
             <div class="dataPartida">
@@ -35,19 +36,44 @@ function apostaRapida(button) {
         `;
     }
 
-    // Calcula o retorno esperado com base na odd e no valor apostado
-    let valorApostado = pegarValor(); // Chama a função para pegar o valor apostado
-    if (!isNaN(oddSelecionada) && !isNaN(valorApostado)) {
-        spanRetornoEsperado.innerText = `${oddSelecionada * valorApostado}`; // Exibe o retorno esperado
-    } else {
-        spanRetornoEsperado.innerText = "Valor inválido!";
+    opcaoAposta(button);
+    atualizarRetorno();    
+
+}
+
+function getButtonNumber(button) {
+    let clone = button.cloneNode(true);
+    clone.querySelectorAll("span").forEach(span => span.remove());
+    return parseFloat(clone.textContent.trim()) || 0;
+}
+
+function opcaoAposta(button) {
+    let opcaoAposta = document.getElementById("opcaoAposta");
+
+    if (button.classList.contains("oddsEmpate")) {
+        opcaoAposta.innerText = `em um EMPATE nessa partida`;
+    } else if (button.classList.contains("oddsCasa") || button.classList.contains("oddsFora")) {
+        if (button.classList.length >= 3) {
+            let equipe = Array.from(button.classList).slice(2).join(" ");
+            opcaoAposta.innerText = `na Vitória de ${equipe}`;
+        }
+    } else if (button.classList.contains("oddsAmbasMarcamSim")) {
+        opcaoAposta.innerText = `que AMBAS as equipes marcam`;
+    } else if (button.classList.contains("oddsAmbasMarcamNao")) {
+        opcaoAposta.innerText = `que NO MÁXIMO uma equipe marca`;
+    } else if (button.classList.contains("oddsMaisGols")) {
+        opcaoAposta.innerText = `que sairão MAIS de 3.5 gols na partida`;
+    } else if (button.classList.contains("oddsMenosGols")) {
+        opcaoAposta.innerText = `que sairão MENOS de 3.5 gols na partida`;
     }
 }
 
-function pegarValor() {
-    let valor = document.getElementById("valorApostado").value.trim(); // Obtém o valor apostado
-    let numero = parseInt(valor, 10);  // Converte para inteiro
-    return isNaN(numero) ? 0 : numero; // Retorna 0 caso o valor seja inválido
+document.getElementById("valorApostado").addEventListener("input", atualizarRetorno);
+
+function atualizarRetorno() {
+    let valor = parseFloat(document.getElementById("valorApostado").value) || 0;
+    let retorno = (oddSelecionada * valor);
+    document.getElementById("retornoEsperado").innerText = `₲ ${retorno}`;
 }
 
 function fecharApostaRapida() {
